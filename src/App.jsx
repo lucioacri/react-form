a8298ef064c11dfb0b156fcb3ff446ff7ce78f97
lucioacri <lucioacri@gmail.com>
2025-05-26 15:00:41 +0200

    add on change on input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
 
 function App() {
-  const articlesArray = ["lorem", "ipsum", "dolor", "sit", "amet"];
+  const [articlesArray, setArticlesArray] = useState([
+    "lorem",
+    "ipsum",
+    "dolor",
+    "sit",
+    "amet",
+  ]);
+  const [newArticle, setNewArticle] = useState("");
 
   return (
     <main>
-      <div className="container">
+      <div className="container text-center">
         <div className="row">
           <div className="col">
+            <form>
+              <input
+                type="text"
+                className="form-control"
+                value={newArticle}
+                onChange={(e) => setNewArticle(e.target.value)}
+              />
+              <button className="btn btn-primary">Add</button>
+            </form>
+          </div>
+          <div className="col">
             <ul>
-              {articlesArray.map((article) => (
-                <li>{article}</li>
+              {articlesArray.map((article, index) => (
+                <li key={index}>{article}</li>
               ))}
             </ul>
           </div>
