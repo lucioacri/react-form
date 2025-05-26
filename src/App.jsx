9b3d01566931af28a57ade7f0113950e0abcb770
lucioacri <lucioacri@gmail.com>
2025-05-26 14:42:49 +0200

    add articles array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
 function App() {
-  return;
+  const articlesArray = ["lorem", "ipsum", "dolor", "sit", "amet"];
+
+  return (
+    <main>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul>
+              {articlesArray.map((article) => (
+                <li>{article}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
